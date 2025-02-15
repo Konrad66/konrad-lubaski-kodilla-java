1f353b83a5bca713874ba8da35c44f206cb44b5c
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2025-02-15 13:45:00 +0100

    7.4 add test fo peopleQuantity

--- kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -2,6 +2,7 @@ package com.kodilla.stream.world;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,11 +13,23 @@ class WorldTestSuite {
     @Test
     public void testGetPeopleQuantity(){
         //Given
-        List<World> continents = new ArrayList<>();
+        World earth = new World();
+        Continent europe = new Continent();
+        Continent america = new Continent();
+        Continent africa = new Continent();
+        america.addCountry(new Country(new BigDecimal("99999999901234567890")));
+        africa.addCountry(new Country(new BigDecimal("98765432101234567890")));
+        europe.addCountry(new Country(new BigDecimal("12345678901234567890")));
+        earth.addContinent(europe);
+        earth.addContinent(africa);
+        earth.addContinent(america);
 
         //When
+        BigDecimal peopleQuantity = earth.getPeopleQuantity();
 
         //Then
+        BigDecimal expectedCountry = new BigDecimal("211111110903703703670");
+        assertEquals(expectedCountry, peopleQuantity);
     }
 
 }
